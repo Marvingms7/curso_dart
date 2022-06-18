47787f6b0338c3d3c22e049e99ded4fec6390f64
Marcus Vinicius 07 <69761977+Marvingms7@users.noreply.github.com>
2022-06-18 10:09:40 -0300

    dinamico e opcional

--- funcao/dinamico.dart
+++ funcao/dinamico.dart
@@ -1,1 +1,11 @@
-main() {}
+main() {
+  juntar(27, 3);
+  juntar('Bom dia ', 'galerinha!!');
+  var resultado = juntar('O valor de PI é ', 3.1415);
+  print(resultado.toUpperCase());
+}
+
+String juntar(dynamic a, b) {
+  print(a.toString() + b.toString());
+  return a.toString() + b.toString();
+}
