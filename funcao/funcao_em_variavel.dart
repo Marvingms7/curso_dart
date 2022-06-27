ccd49466529cf1adc5ef4f69b80c5145634c9211
Marcus Vinicius 07 <69761977+Marvingms7@users.noreply.github.com>
2022-06-27 09:35:22 -0300

    Estudando

--- funcao/funcao_em_variavel.dart
+++ funcao/funcao_em_variavel.dart
@@ -1,8 +1,21 @@
 main() {
   // int a = 10;
   // int b = 5;
+  // Tipo, nome e valor.
   int Function(int, int) soma = soma1;
   print(soma(2, 10));
+  //###########################################
+  int Function(int, int) soma2 = (int y, int x) {
+    return y + x;
+  };
+  print(soma2(3, 20));
+  //###########################################
+  var soma3 = ([int x = 10, int y = 10]) {
+    return x + y;
+  };
+  print(soma3(4, 4));
+  print(soma3());
+  print(soma3(12));
 }
 
 int soma1(int a, int b) {
---
+++ funcao/funcao_em_variavel2.dart
@@ -0,0 +1,9 @@
+main() {
+  var adicao = (int a, int b) {
+    return a + b;
+  };
+  print(adicao(3, 3));
+  //#############################
+  var subtracao = (int a, int b) => a - b;
+  print(subtracao(5, 2));
+}
